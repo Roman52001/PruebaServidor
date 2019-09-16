f1064c5fbef101675224872ad2dd715c2ee7d929
cristian <chtello05@gmail.com>
2019-09-16 10:28:58 -0600

    dbconnection

--- Desktop/ServidorNode/index.js
+++ Desktop/ServidorNode/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const mysql= require('mysql')
 
 const routes = require('./routes');
 const routesApi = require('./routes-api');
+ 
+ var bodyParser = require('body-parser');
+
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({
+     extended: true
+ }));
+
+//dbconnection
+// connection configurations
+ var db = mysql.createConnection({
+     host: '34.70.226.179',
+     port: '3306',
+     user: 'root',
+     password: 'practicante',
+     database: 'dbproyecto'
+ });
+ // connect to database
+ db.connect(); 
+
 
 //middlewares
 
@@ -16,6 +37,14 @@ app.use(morgan('dev'));
 //rutas
 app.use(routes);
 app.use('/api', routesApi);
+
+//get all the users from database
+ app.get('/empleados', function (req, res) {
+     db.query('SELECT * FROM empleado', function (error, results, fields) {
+         if (error) throw error;
+         return res.json(results); 
+     });
+ });
 
 app.get('*', (req, res) => {
     res.end('Archivo No Encontrado');
